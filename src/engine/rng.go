35d9276bb8227b55c5d333e0e14a0c1254c86a13
Andrew J. McGehee <andrewjmcgehee.us@gmail.com>
2024-02-11 10:14:48 -0600

    refactor global rand state into struct

--- src/engine/rng.go
+++ src/engine/rng.go
@@ -1,21 +1,30 @@
 package engine
 
-var rand_state uint32 = 1804289383
-
-func XORRand() uint32 {
-	number := rand_state
-	number ^= number << 13
-	number ^= number >> 17
-	number ^= number << 5
-	rand_state = number
-	return rand_state
+type RNG struct {
+	random_state uint32
 }
 
-func Rand() uint64 {
+func NewRNG() RNG {
+	return RNG{random_state: 1804289383}
+}
+
+func (r *RNG) XORRand() uint32 {
+	next := r.random_state
+	next ^= next << 13
+	next ^= next >> 17
+	next ^= next << 5
+	r.random_state = next
+	return r.random_state
+}
+
+func (r *RNG) Rand() uint64 {
 	mask := uint32(0xffff)
-	return uint64(XORRand()&mask) | uint64(XORRand()&mask)<<16 | uint64(XORRand()&mask)<<32 | uint64(XORRand()&mask)<<48
+	return (uint64(r.XORRand()&mask) |
+		uint64(r.XORRand()&mask)<<16 |
+		uint64(r.XORRand()&mask)<<32 |
+		uint64(r.XORRand()&mask)<<48)
 }
 
-func MagicCandidate() uint64 {
-	return Rand() & Rand() & Rand()
+func (r *RNG) MagicCandidate() uint64 {
+	return r.Rand() & r.Rand() & r.Rand()
 }
--- src/engine/rng_test.go
+++ src/engine/rng_test.go
@@ -2,28 +2,26 @@ package engine
 
 import "testing"
 
-var test_rand_state = rand_state
-
 func TestXORRand(t *testing.T) {
-	rand_state = test_rand_state
+	r := NewRNG()
 	expected := uint32(0x0000000067d33a74)
-	if actual := XORRand(); expected != actual {
+	if actual := r.XORRand(); expected != actual {
 		t.Errorf("XORRand() first call should return 0x%016x but returned 0x%016x", expected, actual)
 	}
 }
 
 func TestRand(t *testing.T) {
-	rand_state = test_rand_state
+	r := NewRNG()
 	expected := uint64(0x41e0fc38fd7a3a74)
-	if actual := Rand(); expected != actual {
+	if actual := r.Rand(); expected != actual {
 		t.Errorf("Rand() first call should return 0x%016x but returned 0x%016x", expected, actual)
 	}
 }
 
 func TestMagicCandidate(t *testing.T) {
-	rand_state = test_rand_state
+	r := NewRNG()
 	expected := uint64(0x0060800004203010)
-	if actual := MagicCandidate(); expected != actual {
+	if actual := r.MagicCandidate(); expected != actual {
 		t.Errorf("MagicCandidate() first call should return 0x%016x but returned 0x%016x", expected, actual)
 	}
 }
