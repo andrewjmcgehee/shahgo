705d6e3099d26c796d476b4862ed5c1ca1502952
Andrew J. McGehee <andrewjmcgehee.us@gmail.com>
2024-07-07 17:26:58 -0500

    updated Sun Jul  7 17:26:58 CDT 2024

--- src/main.go
+++ src/main.go
@@ -7,8 +7,8 @@ import (
 // just a driver for random testing for now
 func main() {
 	engine.InitAttacks()
-	b := engine.ParseFEN(engine.FenEmpty)
-	// b := engine.ParseFEN(engine.FenStart)
+	// b := engine.ParseFEN(engine.FenEmpty)
+	b := engine.ParseFEN(engine.FenStart)
 	// b := engine.ParseFEN(engine.FenTricky)
 	// b := engine.ParseFEN(engine.FenKill)
 	// b := engine.ParseFEN(engine.FenCMK)
@@ -18,7 +18,8 @@ func main() {
 	engine.Move(b, engine.Pawn, engine.D2, engine.D4)
 	engine.Move(b, engine.Pawn, engine.F7, engine.F5)
 	engine.Move(b, engine.Pawn, engine.C2, engine.C4)
-	engine.DisplayBitboard(engine.MaskBishopAttacks(engine.C1), true)
+	engine.Move(b, engine.Pawn, engine.F5, engine.F4)
+	engine.DisplayBitboard(engine.MaskRookAttacksWithOccupancy(engine.C1, b.OccupancyBitboards[engine.Both]), true)
 
 	if b.OccupancyBitboards[engine.Both] != b.OccupancyBitboards[engine.White]|b.OccupancyBitboards[engine.Black] {
 		panic("blah")
